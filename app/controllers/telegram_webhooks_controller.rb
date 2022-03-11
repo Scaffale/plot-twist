3b1e726f2df597ac8447453977d2bde91f4d3bae
Martino <martino.giacchetti@gmail.com>
2022-03-11 17:55:20 +0100

    Aggiunto rubocop :cop:

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,10 +1,11 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def inline_query(query, offset)
-    answer_inline_query {
+    result = {
         type: 'mpeg4_gif',
         # id: uniq_id(result.new_name(extra_params)),
         mpeg4_url: root_url + '/gif/domani_ti_spo.gif' # ,
         # thumb_url: [ENV['SERVER_URL'], 'placeholder.jpg'].join('/')
-      }, 0
+      }
+    answer_inline_query result, 0
   end
 end
--- spec/requests/telegram_webhooks_controller_spec.rb
+++ spec/requests/telegram_webhooks_controller_spec.rb
@@ -1,6 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe TelegramWebhooksController, telegram_bot: :rails, type: :request do
   describe "inline_query" do
+    context "when empty query" do
+    end
   end
 end
