0c186261811f829c1a6cda4b02bbe25796f5b1bf
Martino <martino.giacchetti@gmail.com>
2022-03-18 15:12:32 +0100

    doh

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -3,7 +3,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     result = [{
         type: 'mpeg4_gif',
         id: '1234567',
-        mpeg4_url: 'https://futurebots.it/plot-bot' + '/gif/domani_ti_spo.gif' # ,
+        mpeg4_url: 'https://futurebots.it/plot-bot' + '/gif/domani_ti_spo.gif',
         thumb_url: 'https://futurebots.it/plot-bot' + '/placeholder.jpg'
       }]
     answer_inline_query(result, { next_offset: 0 })
