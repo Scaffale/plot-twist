d45a16c0f0fae06b006c5dbf02a6b6febe926165
mgiacchetti <mgiacchetti@fbk.eu>
2023-02-07 17:07:36 +0100

    small changes

--- docker/production_start.sh
+++ docker/production_start.sh
@@ -3,6 +3,7 @@
 # Prepare Database
 echo "Preparing DB"
 RAILS_ENV=production bin/rails db:prepare
+RAILS_ENV=production bin/rails db:seed
 
 bundle exec passenger start --port=80 --app-type rack --environment production
 
